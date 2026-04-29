3f9ac62bcdadb61e8b6dd90975cd2013dad3573f
Alex <amin.necsus@gmail.com>
2026-04-29 20:05:24 +0300

    Add: find-minimum-in-rotated-sorted-array - submission-4

---
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-4.py
@@ -0,0 +1,20 @@
+import random
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        # if n is rotating times that means moves the last n elements to the beginning.
+        # O(log n) time required, its basically writing your own min() function
+        # list has been rotated between 1 and n times
+        # I am thinking abt doing first guess random and then doin l, r, m  algo.
+        if nums[0] == min(nums): return nums[0]
+        # the problem with this solution , it takes memory to create these instead of keeping it O(1)
+        l, r = 0, len(nums) - 1
+        m = random.randint(1, len(nums)-1)
+        while l <= r:
+            if nums[m] < min(nums):
+                l = m + 1
+            elif nums[m] > min(nums):
+                r = m - 1
+            else:
+                return nums[m]
+            m = l + ((r - l) // 2)
+        return min(nums)
