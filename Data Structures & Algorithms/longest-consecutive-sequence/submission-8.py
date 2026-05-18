1bbac5c41c6dc2b81c6fcc4e71775ce360889c6d
Obukhov Amin <amin.necsus@gmail.com>
2026-05-18 13:34:56 +0300

    Add: longest-consecutive-sequence - submission-8

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-8.py
@@ -0,0 +1,23 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        """
+        1'st idea: brute force, 2nd is: find considerable number and then start adding one while checking
+        and while checking we'd like to count length of string and if its bigger than half return it.
+        its enough to just count value if its consecutive or not.
+        """
+        # actully theres one more solution: sorting!
+        if nums == []: return 0
+        # iteration:
+        collector = 0
+        check = set(nums)
+        count = 1
+        for n in check:
+            if n - 1 in check:
+                continue
+            else:
+                while n + 1 in check:
+                    count += 1
+                    n += 1
+                collector = max(collector, count)
+                count = 1
+        return collector
