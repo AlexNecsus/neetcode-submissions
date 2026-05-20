afb64554d3dcfbcb347bafba7c9e5a69e8a4b782
Obukhov Amin <amin.necsus@gmail.com>
2026-05-20 23:06:24 +0300

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.py
@@ -0,0 +1,28 @@
+class MinStack:
+    # what if we comparing previous number and putting the most minimul value on top of the stack
+    def __init__(self):
+        self.stack = []
+        self.minimum = []
+
+    def push(self, val: int) -> None:
+        if self.minimum:
+            if self.minimum[-1] > val:
+                self.minimum.append(val)
+            else:
+                self.minimum.append(self.minimum[-1])
+        else:
+            self.minimum.append(val)
+        self.stack.append(val)
+
+    def pop(self) -> None:
+        self.minimum.pop()
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        if self.minimum:
+            return self.minimum[-1]
+        else:
+            return int()
